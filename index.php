3b49ee605b38e805b406ba6b987e37d45d9b5e7b
pro-mannan <promasudbd@gmail.com>
2023-09-07 16:15:27 +0600

    raw php first form project

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Home Page</title>
+    <!-- google font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <!-- normalize file -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <!-- milligram file -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+    <!-- CSS Main File -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+    <h1>Hello World</h1>
+    
+</body>
+</html>
